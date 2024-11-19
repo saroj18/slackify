faee109f8d16de6f31cad4e5a51d5a1747f4edb3
saroj <dev.saroj018@gmail.com>
2024-11-19 12:57:24 +0545

    feat: enhance WhiteBoard component with user-specific access control and workspace data fetching

--- src/app/_components/whiteboard/whiteboard.tsx
+++ src/app/_components/whiteboard/whiteboard.tsx
@@ -1,16 +1,52 @@
 "use client";
 
-import { Tldraw } from "tldraw";
+import { Tldraw, TLUiComponents } from "tldraw";
 import "tldraw/tldraw.css";
 import { useSyncDemo } from "@tldraw/sync";
 import { useParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 
-export default function WhiteBoard() {
+const components: Required<TLUiComponents> = {
+  ContextMenu: null,
+  ActionsMenu: null,
+  HelpMenu: null,
+  ZoomMenu: null,
+  MainMenu: null,
+  Minimap: null,
+  StylePanel: null,
+  PageMenu: null,
+  NavigationPanel: null,
+  Toolbar: null,
+  KeyboardShortcutsDialog: null,
+  QuickActions: null,
+  HelperButtons: null,
+  DebugPanel: null,
+  DebugMenu: null,
+  SharePanel: null,
+  MenuPanel: null,
+  TopPanel: null,
+  CursorChatBubble: null,
+};
+
+export default function WhiteBoard({ workspace }: { workspace: any }) {
   const { id } = useParams();
   const store = useSyncDemo({ roomId: `channel-${id}` });
+  const { data } = useSession();
   return (
-    <div className="tldraw__editor max-h-[635px] h-full">
-      <Tldraw store={store} />
+    <div
+      className={`tldraw__editor  max-h-[635px] h-full ${
+        workspace?.data?.createdBy !== data?.user.id
+          ? "pointer-events-none"
+          : ""
+      }`}
+    >
+      <Tldraw
+        components={
+          (workspace?.data?.createdBy !== data?.user.id && components) ||
+          undefined
+        }
+        store={store}
+      />
     </div>
   );
 }
--- src/app/workspace/[workspace]/channel/[id]/page.tsx
+++ src/app/workspace/[workspace]/channel/[id]/page.tsx
@@ -7,23 +7,22 @@ import WhiteBoard from "@/app/_components/whiteboard/whiteboard";
 import { useParams } from "next/navigation";
 import Navbar from "./_components/nav";
 import ChatPage from "./pages/chat-page";
-import { PUSHER_CLIENT } from "@/utils/pusher";
-import { JSONContent } from "novel";
 import CanvasPage from "./pages/canvas-page";
 import CanvasPreviewPage from "./pages/canvas-preview-page";
 
 export default function page() {
   const [state, setState] = useState("Message");
-  // const [workspace, setWorkspace] = useState<any>(null);
+  const [workspace, setWorkspace] = useState<any>(null);
+  const { workspace: workspaceId } = useParams();
 
   useEffect(() => {
-    // const getIWorkSpace = async () => {
-    //   const res = await fetch("/api/workspace/" + id);
-    //   const data = await res.json();
-    //   console.log(data);
-    //   setWorkspace(data);
-    // };
-    // getIWorkSpace();
+    const getIWorkSpace = async () => {
+      const res = await fetch("/api/workspace/" + workspaceId);
+      const data = await res.json();
+      console.log(data);
+      setWorkspace(data);
+    };
+    getIWorkSpace();
   }, []);
 
   return (
@@ -32,7 +31,7 @@ export default function page() {
       {state == "Message" && <ChatPage />}
       {state == "Canvas" && <CanvasPage />}
       {state == "Canvas Preview" && <CanvasPreviewPage />}
-      {state == "WhiteBoard" && <WhiteBoard />}
+      {state == "WhiteBoard" && <WhiteBoard workspace={workspace} />}
     </div>
   );
 }
